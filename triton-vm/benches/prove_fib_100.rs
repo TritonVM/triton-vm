1326b12dd18aac6b1420a28aef725bb48a4ec14b
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2022-11-15 02:38:48 +0100

    add benchmark for STARK proving

---
+++ triton-vm/benches/prove_fib_100.rs
@@ -0,0 +1,66 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+
+use triton_profiler::prof_start;
+use triton_profiler::prof_stop;
+use triton_profiler::triton_profiler::{Report, TritonProfiler};
+use triton_vm::instruction::sample_programs;
+use triton_vm::proof::Claim;
+use triton_vm::stark::Stark;
+use triton_vm::vm::Program;
+
+/// cargo criterion --bench prove_fib_100
+fn prove_fib_100(criterion: &mut Criterion) {
+    let mut group = criterion.benchmark_group("prove_fib_100");
+    group.sample_size(10); // runs
+
+    let fib_100 = BenchmarkId::new("ProveFib100", 0);
+
+    let mut maybe_profiler = Some(TritonProfiler::new("Prove Fibonacci 100"));
+    let mut report: Report = Report::placeholder();
+
+    // stark object
+    let program = match Program::from_code(sample_programs::FIBONACCI_VIT) {
+        Err(e) => panic!("Cannot compile source code into program: {}", e),
+        Ok(p) => p,
+    };
+    let input = vec![100_u64.into()];
+    let (aet, output, err) = program.simulate(input.clone(), vec![]);
+    if let Some(error) = err {
+        panic!("The VM encountered the following problem: {}", error);
+    }
+
+    let claim = Claim {
+        input,
+        program: program.to_bwords(),
+        output,
+        padded_height: 0,
+    };
+    let stark = Stark::new(claim, Default::default());
+
+    group.bench_function(fib_100, |bencher| {
+        bencher.iter(|| {
+            prof_start!(maybe_profiler, "prove");
+            let _proof = stark.prove(aet.clone(), &mut maybe_profiler);
+            prof_stop!(maybe_profiler, "prove");
+
+            if let Some(profiler) = maybe_profiler.as_mut() {
+                profiler.finish();
+                report = profiler.report();
+            }
+            maybe_profiler = None;
+        });
+    });
+
+    group.finish();
+
+    println!("Writing report ...");
+    println!("{}", report);
+}
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default();
+    targets = prove_fib_100
+}
+
+criterion_main!(benches);
