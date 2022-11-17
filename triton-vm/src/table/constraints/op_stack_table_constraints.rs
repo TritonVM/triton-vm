9cbf2010a57f9530bfcba694e0dc13f1a15d2da4
Simon Shine <simon@neptune.cash>
2022-11-17 08:42:57 +0100

    Add placeholder comment to *_table_constraints.rs

--- triton-vm/src/table/constraints/op_stack_table_constraints.rs
+++ triton-vm/src/table/constraints/op_stack_table_constraints.rs
@@ -1,4 +1,7 @@
 use crate::table::extension_table::Evaluable;
 use crate::table::op_stack_table::ExtOpStackTable;
 
+// This file is a placeholder for auto-generated code
+// Run `cargo run --bin constraint-evaluation-generator`
+// to fill in this file with optimized constraints.
 impl Evaluable for ExtOpStackTable {}
--- triton-vm/src/table/constraints/ram_table_constraints.rs
+++ triton-vm/src/table/constraints/ram_table_constraints.rs
@@ -1,4 +1,7 @@
 use crate::table::extension_table::Evaluable;
 use crate::table::ram_table::ExtRamTable;
 
+// This file is a placeholder for auto-generated code
+// Run `cargo run --bin constraint-evaluation-generator`
+// to fill in this file with optimized constraints.
 impl Evaluable for ExtRamTable {}
