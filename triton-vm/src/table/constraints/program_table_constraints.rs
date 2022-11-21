426c206bd290c4d276a31fd8652fe093cd2c5144
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2022-11-21 11:50:34 +0100

    simplify code-template for table's constraints

--- triton-vm/src/table/constraints/program_table_constraints.rs
+++ triton-vm/src/table/constraints/program_table_constraints.rs
@@ -1,7 +1,3 @@
-use twenty_first::shared_math::mpolynomial::Degree;
-use twenty_first::shared_math::x_field_element::XFieldElement;
-
-use crate::table::challenges::AllChallenges;
 use crate::table::extension_table::Evaluable;
 use crate::table::extension_table::Quotientable;
 use crate::table::program_table::ExtProgramTable;
@@ -9,87 +5,6 @@ use crate::table::program_table::ExtProgramTable;
 // This file is a placeholder for auto-generated code
 // Run `cargo run --bin constraint-evaluation-generator`
 // to fill in this file with optimized constraints.
-impl Evaluable for ExtProgramTable {
-    fn evaluate_initial_constraints(
-        &self,
-        _evaluation_point: &[XFieldElement],
-        _challenges: &AllChallenges,
-    ) -> Vec<XFieldElement> {
-        panic!(
-            "Constraints must be in place. Run `cargo run --bin constraint-evaluation-generator`."
-        )
-    }
+impl Evaluable for ExtProgramTable {}
 
-    fn evaluate_consistency_constraints(
-        &self,
-        _evaluation_point: &[XFieldElement],
-        _challenges: &AllChallenges,
-    ) -> Vec<XFieldElement> {
-        panic!(
-            "Constraints must be in place. Run `cargo run --bin constraint-evaluation-generator`."
-        )
-    }
-
-    fn evaluate_transition_constraints(
-        &self,
-        _current_row: &[XFieldElement],
-        _next_row: &[XFieldElement],
-        _challenges: &AllChallenges,
-    ) -> Vec<XFieldElement> {
-        panic!(
-            "Constraints must be in place. Run `cargo run --bin constraint-evaluation-generator`."
-        )
-    }
-
-    fn evaluate_terminal_constraints(
-        &self,
-        _evaluation_point: &[XFieldElement],
-        _challenges: &AllChallenges,
-    ) -> Vec<XFieldElement> {
-        panic!(
-            "Constraints must be in place. Run `cargo run --bin constraint-evaluation-generator`."
-        )
-    }
-}
-
-impl Quotientable for ExtProgramTable {
-    fn get_initial_quotient_degree_bounds(
-        &self,
-        _padded_height: usize,
-        _num_trace_randomizers: usize,
-    ) -> Vec<Degree> {
-        panic!(
-            "Degree bounds must be in place. Run `cargo run --bin constraint-evaluation-generator`."
-        )
-    }
-
-    fn get_consistency_quotient_degree_bounds(
-        &self,
-        _padded_height: usize,
-        _num_trace_randomizers: usize,
-    ) -> Vec<Degree> {
-        panic!(
-            "Degree bounds must be in place. Run `cargo run --bin constraint-evaluation-generator`."
-        )
-    }
-
-    fn get_transition_quotient_degree_bounds(
-        &self,
-        _padded_height: usize,
-        _num_trace_randomizers: usize,
-    ) -> Vec<Degree> {
-        panic!(
-            "Degree bounds must be in place. Run `cargo run --bin constraint-evaluation-generator`."
-        )
-    }
-
-    fn get_terminal_quotient_degree_bounds(
-        &self,
-        _padded_height: usize,
-        _num_trace_randomizers: usize,
-    ) -> Vec<Degree> {
-        panic!(
-            "Degree bounds must be in place. Run `cargo run --bin constraint-evaluation-generator`."
-        )
-    }
-}
+impl Quotientable for ExtProgramTable {}
