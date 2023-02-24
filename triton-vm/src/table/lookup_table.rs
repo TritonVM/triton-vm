e29b108786b3fa97936058bc16372a33e43d27db
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2023-02-24 08:09:35 +0100

    fill Lookup Table

--- triton-vm/src/table/lookup_table.rs
+++ triton-vm/src/table/lookup_table.rs
@@ -1,11 +1,11 @@
 use ndarray::s;
+use ndarray::Array1;
 use ndarray::ArrayView2;
 use ndarray::ArrayViewMut2;
 use strum::EnumCount;
 use twenty_first::shared_math::b_field_element::BFieldElement;
 use twenty_first::shared_math::b_field_element::BFIELD_ONE;
-use twenty_first::shared_math::b_field_element::BFIELD_ZERO;
-use twenty_first::shared_math::tip5::Tip5;
+use twenty_first::shared_math::tip5;
 use twenty_first::shared_math::x_field_element::XFieldElement;
 
 use crate::table::challenges::Challenges;
@@ -15,28 +15,12 @@ use crate::table::constraint_circuit::SingleRowIndicator;
 use crate::table::table_column::LookupBaseTableColumn;
 use crate::table::table_column::LookupBaseTableColumn::*;
 use crate::table::table_column::LookupExtTableColumn;
-use crate::table::table_column::LookupExtTableColumn::*;
+use crate::table::table_column::MasterBaseTableColumn;
 use crate::vm::AlgebraicExecutionTrace;
 
 pub const BASE_WIDTH: usize = LookupBaseTableColumn::COUNT;
 pub const EXT_WIDTH: usize = LookupExtTableColumn::COUNT;
 pub const FULL_WIDTH: usize = BASE_WIDTH + EXT_WIDTH;
-
-/// The lookup table for [Tip5](https://eprint.iacr.org/2023/107.pdf)'s split-and-lookup S-Box,
-/// which uses the Fermat cube map over the finite field with 2^8 + 1 elements.
-pub const LOOKUP_TABLE: [BFieldElement; 1 << 8] = lookup_table();
-
-/// Compute the lookup table for the Fermat cube map. Helper method for the [`LOOKUP_TABLE`],
-/// which should be used instead.
-const fn lookup_table() -> [BFieldElement; 1 << 8] {
-    let mut lookup_table = [BFIELD_ZERO; 1 << 8];
-    let mut i = 0;
-    while i < (1 << 8) {
-        lookup_table[i] = BFieldElement::new(Tip5::LOOKUP_TABLE[i] as u64);
-        i += 1;
-    }
-    lookup_table
-}
 
 pub struct LookupTable {}
 
@@ -47,6 +31,26 @@ impl LookupTable {
         lookup_table: &mut ArrayViewMut2<BFieldElement>,
         aet: &AlgebraicExecutionTrace,
     ) {
+        assert!(lookup_table.nrows() >= 1 << 8);
+
+        let lookup_input = Array1::from_iter((0_u64..1 << 8).map(BFieldElement::new));
+        let lookup_output = Array1::from_iter(
+            (0..1 << 8).map(|i| BFieldElement::new(tip5::LOOKUP_TABLE[i] as u64)),
+        );
+        let lookup_multiplicities = Array1::from_iter(
+            aet.lookup_table_lookup_multiplicities
+                .map(BFieldElement::new),
+        );
+
+        let lookup_input_column =
+            lookup_table.slice_mut(s![..1_usize << 8, LookIn.base_table_index()]);
+        lookup_input.move_into(lookup_input_column);
+        let lookup_output_column =
+            lookup_table.slice_mut(s![..1_usize << 8, LookOut.base_table_index()]);
+        lookup_output.move_into(lookup_output_column);
+        let lookup_multiplicities_column =
+            lookup_table.slice_mut(s![..1_usize << 8, LookupMultiplicity.base_table_index()]);
+        lookup_multiplicities.move_into(lookup_multiplicities_column);
     }
 
     pub fn pad_trace(lookup_table: &mut ArrayViewMut2<BFieldElement>) {
