1e75587947e840ed00c080e8efd5d451f30a4682
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2023-05-04 18:32:31 +0200

    add constraints for mock deterministic columns

--- triton-vm/src/table/degree_lowering_table.rs
+++ triton-vm/src/table/degree_lowering_table.rs
@@ -1,4 +1,5 @@
 use ndarray::s;
+use ndarray::ArrayView1;
 use ndarray::ArrayView2;
 use ndarray::ArrayViewMut2;
 use ndarray::Axis;
@@ -8,8 +9,10 @@ use strum_macros::Display;
 use strum_macros::EnumCount as EnumCountMacro;
 use strum_macros::EnumIter;
 use twenty_first::shared_math::b_field_element::BFieldElement;
+use twenty_first::shared_math::mpolynomial::Degree;
 use twenty_first::shared_math::x_field_element::XFieldElement;
 
+use crate::table::challenges::Challenges;
 use crate::table::extension_table::Evaluable;
 use crate::table::extension_table::Quotientable;
 use crate::table::master_table::NUM_BASE_COLUMNS;
@@ -84,6 +87,144 @@ impl DegreeLoweringTable {
     }
 }
 
-impl Evaluable<BFieldElement> for ExtDegreeLoweringTable {}
-impl Evaluable<XFieldElement> for ExtDegreeLoweringTable {}
-impl Quotientable for ExtDegreeLoweringTable {}
+impl Evaluable<BFieldElement> for ExtDegreeLoweringTable {
+    fn evaluate_initial_constraints(
+        _base_row: ArrayView1<BFieldElement>,
+        _ext_row: ArrayView1<XFieldElement>,
+        _challenges: &Challenges,
+    ) -> Vec<XFieldElement> {
+        vec![]
+    }
+
+    fn evaluate_consistency_constraints(
+        base_row: ArrayView1<BFieldElement>,
+        ext_row: ArrayView1<XFieldElement>,
+        _challenges: &Challenges,
+    ) -> Vec<XFieldElement> {
+        let deterministic_base_section_start = NUM_BASE_COLUMNS - BASE_WIDTH;
+        let deterministic_ext_section_start = NUM_EXT_COLUMNS - EXT_WIDTH;
+
+        let b_dummy_0 = base_row[deterministic_base_section_start] - base_row[0] * base_row[1];
+        let x_dummy_0 = ext_row[deterministic_ext_section_start] - base_row[0] * ext_row[0];
+
+        let base_constraints = [b_dummy_0];
+        let ext_constraints = [x_dummy_0];
+
+        base_constraints
+            .map(|b| b.lift())
+            .into_iter()
+            .chain(ext_constraints)
+            .collect()
+    }
+
+    fn evaluate_transition_constraints(
+        _current_base_row: ArrayView1<BFieldElement>,
+        _current_ext_row: ArrayView1<XFieldElement>,
+        _next_base_row: ArrayView1<BFieldElement>,
+        _next_ext_row: ArrayView1<XFieldElement>,
+        _challenges: &Challenges,
+    ) -> Vec<XFieldElement> {
+        vec![]
+    }
+
+    fn evaluate_terminal_constraints(
+        _base_row: ArrayView1<BFieldElement>,
+        _ext_row: ArrayView1<XFieldElement>,
+        _challenges: &Challenges,
+    ) -> Vec<XFieldElement> {
+        vec![]
+    }
+}
+
+impl Evaluable<XFieldElement> for ExtDegreeLoweringTable {
+    fn evaluate_initial_constraints(
+        _base_row: ArrayView1<XFieldElement>,
+        _ext_row: ArrayView1<XFieldElement>,
+        _challenges: &Challenges,
+    ) -> Vec<XFieldElement> {
+        vec![]
+    }
+
+    fn evaluate_consistency_constraints(
+        base_row: ArrayView1<XFieldElement>,
+        ext_row: ArrayView1<XFieldElement>,
+        _challenges: &Challenges,
+    ) -> Vec<XFieldElement> {
+        let deterministic_base_section_start = NUM_BASE_COLUMNS - BASE_WIDTH;
+        let deterministic_ext_section_start = NUM_EXT_COLUMNS - EXT_WIDTH;
+
+        let b_dummy_0 = base_row[deterministic_base_section_start] - base_row[0] * base_row[1];
+        let x_dummy_0 = ext_row[deterministic_ext_section_start] - base_row[0] * ext_row[0];
+
+        let base_constraints = [b_dummy_0];
+        let ext_constraints = [x_dummy_0];
+
+        base_constraints
+            .into_iter()
+            .chain(ext_constraints)
+            .collect()
+    }
+
+    fn evaluate_transition_constraints(
+        _current_base_row: ArrayView1<XFieldElement>,
+        _current_ext_row: ArrayView1<XFieldElement>,
+        _next_base_row: ArrayView1<XFieldElement>,
+        _next_ext_row: ArrayView1<XFieldElement>,
+        _challenges: &Challenges,
+    ) -> Vec<XFieldElement> {
+        vec![]
+    }
+
+    fn evaluate_terminal_constraints(
+        _base_row: ArrayView1<XFieldElement>,
+        _ext_row: ArrayView1<XFieldElement>,
+        _challenges: &Challenges,
+    ) -> Vec<XFieldElement> {
+        vec![]
+    }
+}
+
+impl Quotientable for ExtDegreeLoweringTable {
+    fn num_initial_quotients() -> usize {
+        0
+    }
+
+    fn num_consistency_quotients() -> usize {
+        2
+    }
+
+    fn num_transition_quotients() -> usize {
+        0
+    }
+
+    fn num_terminal_quotients() -> usize {
+        0
+    }
+
+    fn initial_quotient_degree_bounds(_interpolant_degree: Degree) -> Vec<Degree> {
+        vec![]
+    }
+
+    fn consistency_quotient_degree_bounds(
+        interpolant_degree: Degree,
+        padded_height: usize,
+    ) -> Vec<Degree> {
+        let zerofier_degree = padded_height as Degree;
+        [
+            interpolant_degree * 2 - zerofier_degree,
+            interpolant_degree * 2 - zerofier_degree,
+        ]
+        .to_vec()
+    }
+
+    fn transition_quotient_degree_bounds(
+        _interpolant_degree: Degree,
+        _padded_height: usize,
+    ) -> Vec<Degree> {
+        vec![]
+    }
+
+    fn terminal_quotient_degree_bounds(_interpolant_degree: Degree) -> Vec<Degree> {
+        vec![]
+    }
+}
