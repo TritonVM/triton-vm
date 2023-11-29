8301d5db375a3d75ed72b3ef92bbe1c4addd4f94
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2023-11-29 11:01:02 +0100

    feat: add benchmark just executing a Triton VM program

--- triton-vm/benches/trace_mmr_new_peak_calculation.rs
+++ triton-vm/benches/trace_mmr_new_peak_calculation.rs
@@ -6,7 +6,17 @@ criterion_main!(benches);
 criterion_group! {
     name = benches;
     config = Criterion::default();
-    targets = trace_mmr_new_peak_calculation
+    targets = run_mmr_new_peak_calculation, trace_mmr_new_peak_calculation
+}
+
+fn run_mmr_new_peak_calculation(criterion: &mut Criterion) {
+    let program = example_programs::CALCULATE_NEW_MMR_PEAKS_FROM_APPEND_WITH_SAFE_LISTS.clone();
+
+    criterion.bench_function("Run finding new peaks for MMR", |bencher| {
+        bencher.iter(|| {
+            program.run([].into(), [].into()).unwrap();
+        });
+    });
 }
 
 fn trace_mmr_new_peak_calculation(criterion: &mut Criterion) {
