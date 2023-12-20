dd47c059eae778932f605e471d212752a9729047
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2023-12-20 20:33:58 +0100

    increase timeout of child process termination

--- triton-tui/tests/tests.rs
+++ triton-tui/tests/tests.rs
@@ -7,18 +7,20 @@ use rexpect::spawn;
 
 #[test]
 fn setup_and_shutdown_triton_tui_with_trivial_program() {
-    let mut child = setup_and_start_triton_tui_with_trivial_program();
+    let timeout = Some(180_000);
+    let mut child = setup_and_start_triton_tui_with_trivial_program(timeout);
     let_assert!(Ok(_) = child.send_line("q"));
     let_assert!(Ok(_) = child.exp_eof());
 }
 
 #[test]
 fn setup_without_shutdown_of_triton_tui_with_trivial_program_leaves_tui_open() {
-    let mut child = setup_and_start_triton_tui_with_trivial_program();
+    let timeout = Some(10_000);
+    let mut child = setup_and_start_triton_tui_with_trivial_program(timeout);
     let_assert!(Err(Error::Timeout { .. }) = child.exp_eof());
 }
 
-fn setup_and_start_triton_tui_with_trivial_program() -> PtySession {
+fn setup_and_start_triton_tui_with_trivial_program(timeout: Option<u64>) -> PtySession {
     let path_to_trivial_program = manifest_dir().join("tests/trivial_program.tasm");
 
     // does file exist?
@@ -26,7 +28,6 @@ fn setup_and_start_triton_tui_with_trivial_program() -> PtySession {
     let_assert!(Some(path_to_trivial_program) = path_to_trivial_program.to_str());
 
     let command = format!("cargo run --offline --bin triton-tui -- -p {path_to_trivial_program}");
-    let timeout = Some(5_000);
     let_assert!(Ok(child) = spawn(&command, timeout));
     child
 }
