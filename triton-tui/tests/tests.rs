a9086cc9012a495310911457f1add705746facfd
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2023-12-20 20:14:36 +0100

    use `rexpect` for integration test

--- triton-tui/tests/tests.rs
+++ triton-tui/tests/tests.rs
@@ -1,35 +1,37 @@
-use std::io::Write;
 use std::path::PathBuf;
 
-use assert2::assert;
 use assert2::let_assert;
-use pty_process::blocking::Command;
-use pty_process::blocking::Pty;
-use pty_process::Size;
+use rexpect::error::Error;
+use rexpect::session::PtySession;
+use rexpect::spawn;
+
+#[test]
+fn setup_and_shutdown_triton_tui_with_trivial_program() {
+    let mut child = setup_and_start_triton_tui_with_trivial_program();
+    let_assert!(Ok(_) = child.send_line("q"));
+    let_assert!(Ok(_) = child.exp_eof());
+}
+
+#[test]
+fn setup_without_shutdown_of_triton_tui_with_trivial_program_leaves_tui_open() {
+    let mut child = setup_and_start_triton_tui_with_trivial_program();
+    let_assert!(Err(Error::Timeout { .. }) = child.exp_eof());
+}
+
+fn setup_and_start_triton_tui_with_trivial_program() -> PtySession {
+    let path_to_trivial_program = manifest_dir().join("tests/trivial_program.tasm");
+
+    // does file exist?
+    let_assert!(Ok(_) = std::fs::metadata(&path_to_trivial_program));
+    let_assert!(Some(path_to_trivial_program) = path_to_trivial_program.to_str());
+
+    let command = format!("cargo run --offline --bin triton-tui -- -p {path_to_trivial_program}");
+    let timeout = Some(5_000);
+    let_assert!(Ok(child) = spawn(&command, timeout));
+    child
+}
 
 /// The directory containing the Cargo.toml file.
 fn manifest_dir() -> PathBuf {
     PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-}
-
-#[test]
-#[ignore = "breaks code-coverage tool `cargo-tarpaulin`"]
-fn setup_and_shutdown_triton_tui_with_trivial_program() {
-    let path_to_trivial_program = manifest_dir().join("tests/trivial_program.tasm");
-    let_assert!(Some(path_to_trivial_program) = path_to_trivial_program.to_str());
-    println!("trying to load program from: {path_to_trivial_program}");
-
-    let mut pty = Pty::new().unwrap();
-    let_assert!(Ok(()) = pty.resize(Size::new(24, 80)));
-    let_assert!(Ok(pts) = pty.pts());
-
-    let mut cmd = Command::new("cargo");
-    cmd.args(["run", "--bin", "triton-tui", "--"]);
-    cmd.args(["-p", path_to_trivial_program]);
-    let_assert!(Ok(mut child) = cmd.spawn(&pts));
-
-    let_assert!(Ok(()) = write!(pty, "q"));
-
-    let_assert!(Ok(exit_status) = child.wait());
-    assert!(Some(0) == exit_status.code());
 }
