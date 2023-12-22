aeb059821980a7a5709bac9ed2bf5f58ab75d18a
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2023-12-22 12:01:31 +0100

    test running triton-tui with given initial state

--- triton-tui/tests/tests.rs
+++ triton-tui/tests/tests.rs
@@ -27,7 +27,33 @@ fn setup_and_start_triton_tui_with_trivial_program(timeout: Option<u64>) -> PtyS
     assert!(path_to_trivial_program.exists());
     let_assert!(Some(path_to_trivial_program) = path_to_trivial_program.to_str());
 
-    let command = format!("cargo run --offline --bin triton-tui -- -p {path_to_trivial_program}");
+    let command = format!("cargo run --offline --bin triton-tui -- {path_to_trivial_program}");
+    let_assert!(Ok(child) = spawn(&command, timeout));
+    child
+}
+
+#[test]
+#[ignore = "breaks code-coverage tool `cargo-tarpaulin`"]
+fn setup_and_shutdown_triton_tui_with_example_initial_state() {
+    let timeout = Some(180_000);
+    let mut child = setup_and_start_triton_tui_with_example_initial_state(timeout);
+    let_assert!(Ok(_) = child.send_line("q"));
+    let_assert!(Ok(_) = child.exp_eof());
+}
+
+fn setup_and_start_triton_tui_with_example_initial_state(timeout: Option<u64>) -> PtySession {
+    let path_to_trivial_program = manifest_dir().join("examples/program.tasm");
+    assert!(path_to_trivial_program.exists());
+    let_assert!(Some(path_to_trivial_program) = path_to_trivial_program.to_str());
+
+    let path_to_initial_state = manifest_dir().join("examples/initial_state.json");
+    assert!(path_to_initial_state.exists());
+    let_assert!(Some(path_to_initial_state) = path_to_initial_state.to_str());
+
+    let command = format!(
+        "cargo run --offline --bin triton-tui -- \
+        {path_to_trivial_program} --initial_state {path_to_initial_state}"
+    );
     let_assert!(Ok(child) = spawn(&command, timeout));
     child
 }
