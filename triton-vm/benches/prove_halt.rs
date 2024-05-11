baa6a69b6eaf8581378a431c5977c7f31c1425c8
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2024-05-11 22:03:45 +0200

    refactor(benchmark): report on loops in benchmark

--- triton-vm/benches/prove_halt.rs
+++ triton-vm/benches/prove_halt.rs
@@ -1,33 +1,38 @@
 use criterion::criterion_group;
 use criterion::criterion_main;
-use criterion::BenchmarkId;
 use criterion::Criterion;
 
+use triton_vm::prelude::NonDeterminism;
+use triton_vm::prelude::PublicInput;
 use triton_vm::proof::Claim;
 use triton_vm::stark::Stark;
 use triton_vm::table::master_table::TableId;
 use triton_vm::triton_program;
 
+criterion_main!(benches);
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default().sample_size(10);
+    targets = prove_halt
+}
+
 /// cargo criterion --bench prove_halt
-fn prove_halt(criterion: &mut Criterion) {
+fn prove_halt(c: &mut Criterion) {
     let program = triton_program!(halt);
-    let (aet, output) = program.trace_execution([].into(), [].into()).unwrap();
+    let (aet, output) = program
+        .trace_execution(PublicInput::default(), NonDeterminism::default())
+        .unwrap();
 
     let stark = Stark::default();
     let claim = Claim::about_program(&program).with_output(output);
-    triton_vm::profiler::start("Prove Halt");
     let proof = stark.prove(&claim, &aet).unwrap();
-    let report = triton_vm::profiler::finish();
 
-    let bench_id = BenchmarkId::new("ProveHalt", 0);
-    let mut group = criterion.benchmark_group("prove_halt");
-    group.sample_size(10);
-    group.bench_function(bench_id, |bencher| {
-        bencher.iter(|| {
-            let _proof = stark.prove(&claim, &aet);
-        });
+    triton_vm::profiler::start("Prove Halt");
+    c.bench_function("Prove Halt", |b| {
+        b.iter(|| stark.prove(&claim, &aet).unwrap())
     });
-    group.finish();
+    let report = triton_vm::profiler::finish();
 
     let padded_height = proof.padded_height().unwrap();
     let fri = stark.derive_fri(padded_height).unwrap();
@@ -37,11 +42,3 @@ fn prove_halt(criterion: &mut Criterion) {
         .with_fri_domain_len(fri.domain.length);
     eprintln!("{report}");
 }
-
-criterion_group! {
-    name = benches;
-    config = Criterion::default();
-    targets = prove_halt
-}
-
-criterion_main!(benches);
