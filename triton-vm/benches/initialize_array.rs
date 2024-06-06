ad276c56c18c1544e2f7322b82ee4a81ebff71b8
Alan Szepieniec <alan@neptune.cash>
2024-06-06 14:33:41 +0200

    bench: Add 1d-zeros-into-shape option for array initialization

--- triton-vm/benches/initialize_array.rs
+++ triton-vm/benches/initialize_array.rs
@@ -4,13 +4,19 @@ use criterion::criterion_group;
 use criterion::criterion_main;
 use criterion::Criterion;
 use ndarray::prelude::*;
+use num_traits::One;
 use num_traits::Zero;
+use rand::rngs::StdRng;
+use rand::thread_rng;
+use rand::Rng;
+use rand::RngCore;
+use rand::SeedableRng;
 use twenty_first::prelude::*;
 
 criterion_main!(benches);
 criterion_group!(
     name = benches;
-    config = Criterion::default();
+    config = Criterion::default().sample_size(10);
     targets = init_array<{1 << 10}>,
               init_array<{1 << 12}>,
               init_array<{1 << 14}>,
@@ -18,29 +24,67 @@ criterion_group!(
               init_array<{1 << 18}>,
               init_array<{1 << 20}>,
               init_array<{1 << 22}>,
-              init_array<{1 << 24}>,
 );
 
 fn init_array<const N: usize>(c: &mut Criterion) {
-    let mut group = c.benchmark_group(format!("initialize_array_{N}"));
-    group.bench_function("default", |b| b.iter(|| Array1::<XFieldElement>::zeros(N)));
-    group.bench_function("set_len", |b| b.iter(set_len::<N>));
-    group.bench_function("maybe_uninit", |b| b.iter(maybe_uninit::<N>));
+    const W: usize = 50;
+    let mut group = c.benchmark_group(format!("initialize_array_{N}x{W}"));
+    let mut rng: StdRng = SeedableRng::from_seed(thread_rng().gen());
+    let mut matrix = Array2::<XFieldElement>::zeros((1, 1).f());
+    group.bench_function("zeros", |b| {
+        b.iter(|| {
+            matrix = Array2::<XFieldElement>::zeros((N, W).f());
+            set_ones::<N, W>(&mut rng, &mut matrix);
+        })
+    });
+    group.bench_function("into_shape", |b| {
+        b.iter(|| {
+            matrix = into_shape::<N, W>();
+            set_ones::<N, W>(&mut rng, &mut matrix);
+        })
+    });
+    group.bench_function("set_len", |b| {
+        b.iter(|| {
+            matrix = set_len::<N, W>();
+            set_ones::<N, W>(&mut rng, &mut matrix);
+        })
+    });
+    group.bench_function("maybe_uninit", |b| {
+        b.iter(|| {
+            matrix = maybe_uninit::<N, W>();
+            set_ones::<N, W>(&mut rng, &mut matrix);
+        })
+    });
     group.finish();
 }
 
-#[allow(clippy::uninit_vec)]
-fn set_len<const N: usize>() {
-    let mut the_vec = Vec::with_capacity(N);
-    unsafe {
-        the_vec.set_len(N);
+fn set_ones<const H: usize, const W: usize>(rng: &mut StdRng, matrix: &mut Array2<XFieldElement>) {
+    assert!(!matrix.is_standard_layout());
+    for _ in 0..1000 {
+        let r = (rng.next_u32() as usize) % H;
+        let c = (rng.next_u32() as usize) % W;
+        *matrix.get_mut((r, c)).unwrap() = XFieldElement::one();
     }
-    let mut array = Array1::from_vec(the_vec);
-    array.par_mapv_inplace(|_| XFieldElement::zero());
 }
 
-fn maybe_uninit<const N: usize>() {
-    let mut array = Array1::<XFieldElement>::uninit(N);
+fn into_shape<const N: usize, const W: usize>() -> Array2<XFieldElement> {
+    let array = Array1::<XFieldElement>::zeros(N * W);
+    array.into_shape((W, N)).unwrap().reversed_axes()
+}
+
+#[allow(clippy::uninit_vec)]
+fn set_len<const N: usize, const W: usize>() -> Array2<XFieldElement> {
+    let mut the_vec = Vec::with_capacity(N * W);
+    unsafe {
+        the_vec.set_len(N * W);
+    }
+    let mut array = Array2::from_shape_vec((N, W).f(), the_vec).unwrap();
+    array.par_mapv_inplace(|_| XFieldElement::zero());
+    array
+}
+
+fn maybe_uninit<const N: usize, const W: usize>() -> Array2<XFieldElement> {
+    let mut array = Array2::<XFieldElement>::uninit((N, W).f());
     array.par_mapv_inplace(|_| MaybeUninit::new(XFieldElement::zero()));
-    unsafe { array.assume_init() };
+    unsafe { array.assume_init() }
 }
