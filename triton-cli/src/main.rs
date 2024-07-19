deb224ab6868cd33989753b028e99fe1c31dc794
vimwitch <jchancehud@gmail.com>
2024-07-18 23:41:48 -0700

    refactor: use serde+bincode for serializing

--- triton-cli/src/main.rs
+++ triton-cli/src/main.rs
@@ -1,6 +1,5 @@
 use std::fmt::Write as _;
 use std::fs;
-use std::io::BufReader;
 use std::io::Read;
 use std::io::Write;
 
@@ -8,6 +7,8 @@ use anyhow::Context;
 use anyhow::Result;
 use clap::command;
 use clap::Parser;
+use serde::Deserialize;
+use serde::Serialize;
 use triton_vm::prelude::*;
 
 #[derive(Debug, Parser)]
@@ -113,69 +114,65 @@ fn prove(
         .with_context(|| "Triton VM errored during program execution")?;
     println!("success!");
 
-    // write the proof in an arbitrary binary format
-    //
-    // version: u8
-    //
-    // security_level: u64
-    // fri_expansion_factor: u64
-    // num_trace_randomizers: u64
-    // num_colinearity_checks: u64
-    // num_combination_codeword_checks: u64
-    //
-    // digest_length: u8
-    // public_input_length: u64
-    // public_output_length: u64
-    // proof_length: u64
-    //
-    // program_digest: [u64; digest_length]
-    //
-    // public_inputs: u64[public_input_length]
-    // public_outputs: u64[public_output_length]
-    // proof: u64[proof_length]
     write_proof(data, out).with_context(|| "Failed to write proof to file")?;
     println!("proof written to: {out}");
     Ok(())
 }
 
+#[derive(Serialize, Deserialize)]
+struct SerializedProof {
+    version: u8,
+    security_level: u64,
+    fri_expansion_factor: u64,
+    num_trace_randomizers: u64,
+    num_colinearity_checks: u64,
+    num_combination_codeword_checks: u64,
+    program_digest: Vec<u64>,
+    public_inputs: Vec<u64>,
+    public_outputs: Vec<u64>,
+    proof: Vec<u64>,
+}
+
 fn read_proof(proof_path: &str) -> Result<(Stark, Claim, Proof)> {
-    let file = fs::File::open(proof_path)?;
-    let mut reader = BufReader::new(file);
-    let version = read_u8(&mut reader)?;
-    assert!(version == 1, "wrong proof file version!");
+    let mut file = fs::File::open(proof_path)?;
+    let mut proof_bytes: Vec<u8> = Vec::new();
+    file.read_to_end(&mut proof_bytes)?;
+    let serialized_proof: SerializedProof = bincode::deserialize(&proof_bytes)?;
     let stark = Stark {
-        security_level: read_usize(&mut reader)?,
-        fri_expansion_factor: read_usize(&mut reader)?,
-        num_trace_randomizers: read_usize(&mut reader)?,
-        num_collinearity_checks: read_usize(&mut reader)?,
-        num_combination_codeword_checks: read_usize(&mut reader)?,
+        security_level: usize::try_from(serialized_proof.security_level)?,
+        fri_expansion_factor: usize::try_from(serialized_proof.fri_expansion_factor)?,
+        num_trace_randomizers: usize::try_from(serialized_proof.num_trace_randomizers)?,
+        num_collinearity_checks: usize::try_from(serialized_proof.num_colinearity_checks)?,
+        num_combination_codeword_checks: usize::try_from(
+            serialized_proof.num_combination_codeword_checks,
+        )?,
     };
-    let digest_len = read_u8(&mut reader)?;
-    let input_len = read_u64(&mut reader)?;
-    let output_len = read_u64(&mut reader)?;
-    let proof_len = read_u64(&mut reader)?;
-    let mut d = Digest::default();
+    let mut digest = Digest::default();
     assert_eq!(
-        d.0.len(),
-        usize::from(digest_len),
+        digest.0.len(),
+        serialized_proof.program_digest.len(),
         "digest length mismatch!"
     );
-    read_u64_vec(&mut reader, digest_len.into())?
+    serialized_proof
+        .program_digest
         .iter()
         .enumerate()
-        .for_each(|(i, x)| d.0[i] = BFieldElement::new(*x));
+        .for_each(|(i, x)| digest.0[i] = BFieldElement::new(*x));
     let claim = Claim {
-        program_digest: d,
-        input: read_u64_vec(&mut reader, input_len)?
+        program_digest: digest,
+        input: serialized_proof
+            .public_inputs
             .iter()
             .map(|v| BFieldElement::new(*v))
             .collect(),
-        output: read_u64_vec(&mut reader, output_len)?
+        output: serialized_proof
+            .public_outputs
             .iter()
             .map(|v| BFieldElement::new(*v))
             .collect(),
     };
-    let proof_vec = read_u64_vec(&mut reader, proof_len)?
+    let proof_vec = serialized_proof
+        .proof
         .iter()
         .map(|v| BFieldElement::new(*v))
         .collect();
@@ -184,57 +181,22 @@ fn read_proof(proof_path: &str) -> Result<(Stark, Claim, Proof)> {
 
 fn write_proof(data: (Stark, Claim, Proof), out: &str) -> Result<()> {
     let (stark, claim, proof) = data;
+    let serialized = SerializedProof {
+        version: 1,
+        security_level: u64::try_from(stark.security_level)?,
+        fri_expansion_factor: u64::try_from(stark.fri_expansion_factor)?,
+        num_trace_randomizers: u64::try_from(stark.num_trace_randomizers)?,
+        num_colinearity_checks: u64::try_from(stark.num_collinearity_checks)?,
+        num_combination_codeword_checks: u64::try_from(stark.num_combination_codeword_checks)?,
+        program_digest: claim.program_digest.0.iter().map(u64::from).collect(),
+        public_inputs: claim.input.iter().map(u64::from).collect(),
+        public_outputs: claim.output.iter().map(u64::from).collect(),
+        proof: proof.0.iter().map(u64::from).collect(),
+    };
     let mut file = fs::File::create_new(out)?;
-    file.write_all(&[1])?; // write the version
-                           // fails on systems with usize > 64 bits
-    file.write_all(&u64::try_from(stark.security_level)?.to_le_bytes())?;
-    file.write_all(&u64::try_from(stark.fri_expansion_factor)?.to_le_bytes())?;
-    file.write_all(&u64::try_from(stark.num_trace_randomizers)?.to_le_bytes())?;
-    file.write_all(&u64::try_from(stark.num_collinearity_checks)?.to_le_bytes())?;
-    file.write_all(&u64::try_from(stark.num_combination_codeword_checks)?.to_le_bytes())?;
-
-    file.write_all(&[u8::try_from(claim.program_digest.0.len())?])?;
-    file.write_all(&u64::try_from(claim.input.len())?.to_le_bytes())?;
-    file.write_all(&u64::try_from(claim.output.len())?.to_le_bytes())?;
-    file.write_all(&u64::try_from(proof.0.len())?.to_le_bytes())?;
-    for v in claim.program_digest.0 {
-        file.write_all(&u64::from(&v).to_le_bytes())?;
-    }
-    for v in claim.input {
-        file.write_all(&u64::from(&v).to_le_bytes())?;
-    }
-    for v in claim.output {
-        file.write_all(&u64::from(&v).to_le_bytes())?;
-    }
-    for v in proof.0 {
-        file.write_all(&u64::from(&v).to_le_bytes())?;
-    }
-
+    let proof_bytes = bincode::serialize(&serialized)?;
+    file.write_all(&proof_bytes)?;
     Ok(())
-}
-
-fn read_u64_vec<T: Read>(reader: &mut T, len: u64) -> Result<Vec<u64>> {
-    let mut out = Vec::new();
-    for _ in 0..len {
-        out.push(read_u64(reader)?);
-    }
-    Ok(out)
-}
-
-fn read_u64<T: Read>(reader: &mut T) -> Result<u64> {
-    let mut bytes = [0; 8];
-    reader.read_exact(&mut bytes)?;
-    Ok(u64::from_le_bytes(bytes))
-}
-
-fn read_u8<T: Read>(reader: &mut T) -> Result<u8> {
-    let mut bytes = [0];
-    reader.read_exact(&mut bytes)?;
-    Ok(bytes[0])
-}
-
-fn read_usize<T: Read>(reader: &mut T) -> Result<usize> {
-    Ok(usize::try_from(read_u64(reader)?)?)
 }
 
 #[test]
