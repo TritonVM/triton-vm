0ad31da5be10161b78298454cecc2c89a56efe1d
Jan Ferdinand Sauer <ferdinand@neptune.cash>
2025-05-19 12:40:09 +0200

    WIP: perf: Cut noise in degree lowering flamegraph
    To be used with:
    cargo flamegraph --bench degree_lowering -- --bench
    
    Generating the flamegraph apparently needs a lot of RAM. Should be
    looked at with a powerful machine.

--- triton-constraint-builder/benches/degree_lowering.rs
+++ triton-constraint-builder/benches/degree_lowering.rs
@@ -1,52 +1,15 @@
-use constraint_circuit::ConstraintCircuitMonad;
-use constraint_circuit::InputIndicator;
 use criterion::BatchSize::SmallInput;
-use criterion::BenchmarkGroup;
 use criterion::Criterion;
 use criterion::black_box;
 use criterion::criterion_group;
 use criterion::criterion_main;
-use criterion::measurement::WallTime;
 use triton_constraint_builder::Constraints;
 
 criterion_main!(benches);
 criterion_group! {
     name = benches;
     config = Criterion::default().sample_size(10);
-    targets = assemble_constraints,
-              degree_lower_constraint_types,
-              degree_lower_all,
-}
-
-fn assemble_constraints(criterion: &mut Criterion) {
-    criterion.bench_function("Assemble all constraints", |bencher| {
-        bencher.iter(|| black_box(Constraints::all()))
-    });
-}
-
-fn degree_lower_constraint_types(c: &mut Criterion) {
-    let mut group = c.benchmark_group("lower to target degree");
-    degree_lower_constraints(&mut group, "init", Constraints::initial_constraints());
-    degree_lower_constraints(&mut group, "cons", Constraints::consistency_constraints());
-    degree_lower_constraints(&mut group, "tran", Constraints::transition_constraints());
-    degree_lower_constraints(&mut group, "term", Constraints::terminal_constraints());
-    group.finish();
-}
-
-fn degree_lower_constraints<II: InputIndicator>(
-    group: &mut BenchmarkGroup<WallTime>,
-    constraint_group_name: &str,
-    constraints: Vec<ConstraintCircuitMonad<II>>,
-) {
-    let info = Constraints::default_degree_lowering_info();
-    let bench_name = format!("Degree-lower {constraint_group_name} constraints");
-    group.bench_function(bench_name, |bencher| {
-        bencher.iter_batched(
-            || constraints.clone(),
-            |mut constr| black_box(ConstraintCircuitMonad::lower_to_degree(&mut constr, info)),
-            SmallInput,
-        )
-    });
+    targets = degree_lower_all,
 }
 
 fn degree_lower_all(criterion: &mut Criterion) {
